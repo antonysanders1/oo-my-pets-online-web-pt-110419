7c250701c50222b5bfaa10df26bf250b8107a323
Antony Sanders <antony.devon92@gmail.com>
2019-12-18 00:38:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,18 +9,28 @@ class Owner
   
   
   
+  def self.count
+    self.all.count
+  end 
   
   
   
-  
-  
+  def self.rest_all
+    self.all.clear
+  end 
   
   
   def cat 
   Cat.all.select {|cat| cat.owner == self}
   end
   
+   def cat 
+  Dog.all.select {|dog| dog.owner == self}
+  end
   
+  def buy_cat(name)
+    
+  end 
   
   
   
